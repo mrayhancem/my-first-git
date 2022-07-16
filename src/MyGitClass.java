58e4ada33bef93c1f9eeeaf87788f84c4809bc3f
Cem Ayhan <mr.ayhancem@gmail.com>
2022-07-16 14:39:07 -0500

    third commit

--- src/MyGitClass.java
+++ src/MyGitClass.java
@@ -1,6 +1,10 @@
 public class MyGitClass {
     public static void main(String[] args) {
         System.out.println("Hello");
+        birsey();
+    }
+    public static void birsey(){
+        System.out.println("Bir Sey");
     }
 
 }
